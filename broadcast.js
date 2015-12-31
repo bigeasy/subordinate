db36cc6ee84986494fc8ccb6cec2389ef494dff6
Alan Gutierrez <alan@prettyrobots.com>
2015-12-30 21:44:51 -0500

    Broadcast now operates on an array.

--- broadcast.js
+++ broadcast.js
@@ -1,18 +1,10 @@
 var cadence = require('cadence')
 
+// TODO This is so obvious, probably not worth keeping.
 module.exports = cadence(function (async, transmitters, method, message) {
-    var broadcast = {}
-    async(function () {
-        // TODO Should you run this in parallel? I believe so, since you're
-        // never going to have too many children.
-        async.forEach(function (key) {
-            async(function () {
-                transmitters[key].request(method, message, async())
-            }, function (response) {
-                broadcast[key] = response
-            })
-        })(Object.keys(transmitters))
-    }, function () {
-        return broadcast
-    })
+    // TODO Should you run this in parallel? I believe so, since you're
+    // never going to have too many children.
+    async.map(function (key) {
+        transmitters[key].request(method, message, async())
+    })(transmitters)
 })
