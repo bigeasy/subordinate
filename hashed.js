f25fcb2528ac90045d4488c3a76e7e3c7b1334c9
Alan Gutierrez <alan@prettyrobots.com>
2017-02-25 11:56:46 -0600

    Redux.

--- hashed.js
+++
@@ -1,7 +0,0 @@
-var cadence = require('cadence')
-
-module.exports = cadence(function (async, hash, transmitters, method, key, body) {
-    var buffer = new Buffer(key)
-    var transmitter = transmitters[hash(0, buffer, 0, buffer.length) % transmitters.length]
-    transmitter.request(method, body, async())
-})
