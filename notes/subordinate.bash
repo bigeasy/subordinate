4d64b22279e9fe8478a2df78f84556455ae74d89
Alan Gutierrez <alan@prettyrobots.com>
2017-02-25 11:12:53 -0600

    Upgrade `delta` to 1.2.x.

--- notes/subordinate.bash
+++ notes/subordinate.bash
@@ -4,18 +4,17 @@ prolific stdio syslog --wafer prolific.tcp --bind 8808
 
 subordinate ./tmp/subordinate
 
-subordinate ./tmp/subordinate conduit \
-    prolific tcp://127.0.0.1:8888 \
-    compassion conduit --bind 127.0.0.1:8486
+subordinate ./tmp/subordinate compassion conduit --bind 127.0.0.1:8486
 
-subordinate ./tmp/subordinate mingle \
-    prolific tcp://127.0.0.1:8888 \
-    mingle subordinate
+subordinate ./tmp/subordinate mingle subordinate
 
-subordinate ./tmp/subordinate chaperon \
-    prolific tcp://127.0.0.1:8888 \
-    chaperon --mingle http://127.0.0.1:
+subordinate ./tmp/subordinate chaperon
 
-subordinate ./tmp/subordinate poster --property name=value \
-    prolific tcp://127.0.0.1:8888 \
+subordinate ./tmp/subordinate addendum
+
+subordinate ./tmp/subordinate addendum
+
+subordinate ./tmp/subordinate addendum
+
+subordinate ./tmp/subordinate --property name=value \
     emissary poster --turnstiles 24 --wink http://127.0.0.1:8080
